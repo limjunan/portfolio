229ee0028e1eb28095cf0875c4db73a5c71f9740
limjunan <bryanlim080302@gmail.com>
2024-05-22 19:15:39 +0800

    update contact icons

--- src/components/contact.js
+++ src/components/contact.js
@@ -1,17 +1,30 @@
 import React from "react";
+import { FaGithub } from "react-icons/fa";
+import { FaTelegram } from "react-icons/fa";
+import { FaTelegramPlane } from "react-icons/fa";
+import { FaLinkedin } from "react-icons/fa";
+import { SiGmail } from "react-icons/si";
 
 const Contact = () => {
   return (
-    <div className="h-[30vh] my-16 ml-8 lg:m-20 text-gray-300 flex flex-col">
+    <div className="h-[30vh] mx-8 lg:mx-20 text-gray-300 flex flex-col">
       <span className="font-semibold text-2xl lg:text-4xl">
         Impressed? Get in touch.
       </span>
       <div className="flex items-center justify-center flex-grow">
-        <div className="border-gray-500 rounded-md border p-4 px-16 space-x-4">
-          <span>Gmail</span>
-          <span>LinkedIn</span>
-          <span>Github</span>
-          <span>Telegram</span>
+        <div className="border-gray-500 rounded-md border p-4 px-16 space-x-8 flex">
+          <span>
+            <SiGmail size={32} className="hover:cursor-pointer" />
+          </span>
+          <span>
+            <FaLinkedin size={32} className="hover:cursor-pointer" />
+          </span>
+          <span>
+            <FaGithub size={32} className="hover:cursor-pointer" />
+          </span>
+          <span>
+            <FaTelegramPlane size={32} className="hover:cursor-pointer" />
+          </span>
         </div>
       </div>
     </div>
