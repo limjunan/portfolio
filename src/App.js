316a037d281d603cc9d87f7cc87ff7eadacd120f
limjunan <bryanlim080302@gmail.com>
2024-05-22 19:05:14 +0800

    add footer

--- src/App.js
+++ src/App.js
@@ -2,13 +2,17 @@ import "./App.css";
 import Landing from "./components/landing";
 import Showcase from "./components/showcase";
 import Contact from "./components/contact";
+import Footer from "./components/footer";
 
 function App() {
   return (
     <div className="bg-black">
-      <Landing />
-      <Showcase />
-      <Contact />
+      <div>
+        <Landing />
+        <Showcase />
+        <Contact />
+      </div>
+      <Footer />
     </div>
   );
 }
---
+++ src/components/footer.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Footer = () => {
+  return (
+    <div className="h-[10vh text-gray-300 flex items-center justify-center border-t border-gray-500 p-8 mx-8">
+      <span className="text-sm">
+        © 2024 Bryan Lim. All rights reserved. Singapore.
+      </span>
+    </div>
+  );
+};
+
+export default Footer;
