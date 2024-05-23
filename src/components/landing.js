38e87ea8fc17a7569d708cf5d07bbaa6e3859140
limjunan <bryanlim080302@gmail.com>
2024-05-23 10:55:45 +0800

    update case sensitive image links

--- src/components/landing.js
+++ src/components/landing.js
@@ -23,14 +23,14 @@ const Landing = () => {
       <div
         className="min-h-screen bg-cover bg-center flex flex-col lg:flex-row justify-center items-center lg:justify-start"
         style={{
-          backgroundImage: "url('/background.jpeg')",
+          backgroundImage: "url('background.jpeg')",
         }}
       >
         <div className="text-gray-100 m-8 lg:w-2/5 text-4xl lg:text-5xl lg:ml-20 transition-all duration-500 font-semibold">
           Hi, I'm Bryan Lim. <br />A software engineer.
         </div>
         <img
-          src="/moon.png"
+          src="moon.png"
           alt="Moon"
           className="w-1/2 h-1/2 object-contain object-center"
           style={{ opacity: opacity }}
